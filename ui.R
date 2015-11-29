7e2c67df01c8f0d597571fffa79a4676d58567cb
Rob Kemp <rjameskemp@gmail.com>
2015-11-29 16:53:04 -0700

    added documentation and data/methods

--- ui.R
+++ ui.R
@@ -9,17 +9,20 @@ library(tidyr)
 
 
 dashboardPage(skin="black",
-    dashboardHeader(title= "State KMeans Mapped"),
+    dashboardHeader(title= "State KMeans Mapper"),
     dashboardSidebar(
       sidebarMenu(
         selectInput("clusters","Cluster Number:",
-                    choices=1:30),
+                    choices=1:30, selected = 5),
         menuItem("GitHub Repo", icon = icon("file-code-o"), 
                  href = "https://github.com/robkemp/leaflet_kmeans_demo"))),
-    dashboardBody(fluidRow(box(title="K-Means Map",
-                      leafletOutput("kmeans_map"),
-                      width = 12,
-                      height = 12)))
+    dashboardBody(fluidRow(tabBox(
+                            tabPanel("Median Income and Population K-Means Map",
+                              leafletOutput("kmeans_map")),
+                            tabPanel("Data and Methods", "State-level Population and Median income from the 2013 ACS 5-year File.
+                                  K-means clustering used scaled data and the `kmeans()` call from base R."),
+                           width = 12,
+                           height = 12))
     )
-
+)
 
